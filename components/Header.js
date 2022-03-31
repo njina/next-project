322f472e5fbd255c4278e000cb9ad00fa20e0477
njina <najina@gmail.com>
2022-04-01 01:11:05 +0900

    create board page

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link href="#">
+              <Link href="/src/notice">
                 <a>공지사항</a>
               </Link>
             </li>
