50a388b7dd1dd1b24de7ddacfd9fe27ad36f188d
njina <najina@gmail.com>
2022-03-31 23:54:32 +0900

    install swiper into mainbanner component

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -1,10 +1,30 @@
+import React, { useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
+import { Controller } from "swiper";
+
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/controller";
 
 const MainBanner = () => {
+  const [controlledSwiper, setControlledSwiper] = useState(null);
+
   return (
-    <Swiper spaceBetween={50} slidesPerView={1} onSlideChange={() => console.log("slide change")} onSwiper={(swiper) => console.log(swiper)}>
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Controller]}
+      spaceBetween={50}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      autoplay={{ delay: 2000 }}
+      controller={{ control: controlledSwiper }}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
       <SwiperSlide>
         <Image src="/images/mainbanner_1.jpg" alt="메인비주얼" width="1913" height="790" />
       </SwiperSlide>
