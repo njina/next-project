297353f77aab289361a06004a45e31b077b0c2f5
njina <najina@gmail.com>
2022-04-01 15:55:59 +0900

    add livingroom page

---
+++ pages/src/livingroom.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function Notice({}) {
+  return (
+    <>
+      <Header />
+      <div>거실이고, 내용은 아직 없습니다.</div>
+      <Footer />
+    </>
+  );
+}
